86c8cf5e9d4d22ed296789ac614dd094dc423abe
comfyjase <jay-mottershead@hotmail.com>
2025-01-27 22:39:27 +0000

    Update: Run Settings
    Able to parse settings data and store it.

---
+++ VS2022.DoctestTestAdapter/Settings/DoctestSettingsOutputFileData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS2022.DoctestTestAdapter.Settings
+{
+    public class DoctestSettingsOutputFileData : IEquatable<DoctestSettingsOutputFileData>
+    {
+        private string filePath = "";
+        private string commandArguments = "";
+
+        public string FilePath
+        {
+            get { return filePath; }
+        }
+
+        public string CommandArguments
+        {
+            get { return commandArguments; }
+        }
+
+        public DoctestSettingsOutputFileData(string _filePath, string _commandArguments)
+        {
+            filePath = _filePath;
+            commandArguments = _commandArguments;
+        }
+
+        public bool Equals(DoctestSettingsOutputFileData other)
+        {
+            return 
+            (
+                FilePath.Equals(other.FilePath) &&
+                CommandArguments.Equals(other.CommandArguments)
+            );
+        }
+    }
+}
--- VS2022.DoctestTestAdapter/Settings/DoctestSettingsProvider.cs
+++ VS2022.DoctestTestAdapter/Settings/DoctestSettingsProvider.cs
@@ -3,23 +3,43 @@ using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
+using VS.Common.DoctestTestAdapter;
 
 namespace VS2022.DoctestTestAdapter.Settings
 {
     [SettingsName("DoctestTestAdapter")]
     public class DoctestSettingsProvider : ISettingsProvider
     {
-        private List<string> outputFilePaths = new List<string>();
+        private List<DoctestSettingsOutputFileData> outputFileData = new List<DoctestSettingsOutputFileData>();
 
         public void Load(XmlReader reader)
         {
             Debug.Assert(reader != null);
 
-            //bool movedToOutputFilesAttribute = reader.MoveToAttribute();
+            while (reader.Read())
+            {
+                if (reader.Name.Equals("OutputFile", StringComparison.OrdinalIgnoreCase))
+                {
+                    //Logger.Instance.WriteLine("Node Name: " + reader.Name + " Node Type: " + reader.NodeType.ToString() + " Node Value: " + reader.Value);
+
+                    string filePathAttribute = reader.GetAttribute("FilePath");
+                    string commandArgumentsAttribute = reader.GetAttribute("CommandArguments");
+
+                    // Note, not providing a check for command arguments because these should be optional.
+                    if (!string.IsNullOrEmpty(filePathAttribute))
+                    {
+                        DoctestSettingsOutputFileData fileData = new DoctestSettingsOutputFileData(filePathAttribute, commandArgumentsAttribute);
+
+                        if (!outputFileData.Contains(fileData))
+                        {
+                            outputFileData.Add(fileData);
+
+                            Logger.Instance.WriteLine("Stored output file data FilePath: " + fileData.FilePath + " CommandArguments: " + fileData.CommandArguments);
+                        }
+                    }
+                }
+            }
         }
     }
 }
